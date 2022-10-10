40a0e7feb580a819f6038e2bc3108eb072cf6bf5
Sthefanie <sthefaniegc06@gmail.com>
2022-10-10 15:50:31 -0300

    First Class Position

--- src/applications/Program.java
+++ src/applications/Program.java
@@ -1,10 +1,14 @@
 package applications;
 
+import boardGame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Position pos = new Position(3, 6);
+		
+		System.out.println(pos);
 	}
 
 }
